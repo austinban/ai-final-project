aae9687add04638dfa27f0dfe281401bb78c27cc
Patrick Haller <Patrick Haller>
2016-11-21 19:28:49 -0600

    HTML started

--- grading/bayesian-submissions.py
+++ grading/bayesian-submissions.py
@@ -1,6 +1,8 @@
 import importlib
 import traceback
+import plotly
 from grading.util import roster, print_table
+from utils import isnumber
 # from logic import FolKB
 # from utils import expr
 import os
@@ -45,6 +47,23 @@ def tryOne(label, frame):
                 tjust='rjust',
                 )
     y_pred = fit.predict(frame.data)
+    f = open('helloworld.html', 'w')
+
+    message = """<html>
+    <head></head>
+    <body>"""
+    fakeTheta = []
+    fakeTheta.extend([[('%6.3f' % x) if isnumber(x) else x for x in row]
+            for row in fit.theta_])
+    for i in fakeTheta:
+        for j in i:
+            message += j + '\n'
+
+    message+= """</body>
+    </html>"""
+
+    f.write(message)
+    f.close()
     print("Number of mislabeled points out of a total %d points : %d"
           % (len(frame.data), (frame.target != y_pred).sum()))
 
